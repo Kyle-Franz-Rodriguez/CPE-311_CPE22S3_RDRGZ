272db5f9123eaf09945049f9d885cf06315af716
Kyle-Franz-Rodriguez <qkfprodriguez@tip.edu.ph>
2025-02-01 21:57:59 +0800

    Add files via upload

--- cpe311_firstproblem.py
+++ cpe311_firstproblem.py
@@ -1,43 +1,147 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jan 29 17:11:55 2025
-
-@author: Liv, Kyle
+Created on Thu Jan 30 18:33:25 2025
+@Created by: Liv, Kyle
 """
+class Item:
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+        self.sentient = False
 
-def boat(dictn, n=100):
-    dictn = dict(sorted(dictn.items(), key=lambda x:x[1], reverse=True))
-    for left in dictn.copy():
-        if len(dictn) == 1:
-            if dictn[left] > 100:
-                print(left, "can't cross")
-            else:
-                print([left])
-            break
-        for right in dictn.copy():
-            pass
-        if dictn[left] > n:
-            print(left, "can not cross.")
-            del dictn[left]
-            continue
-        if dictn[right] > n:
-            print(right, "can not cross.")
-            del dictn[right]
-            continue
-        else:
-            partner = dictn[left] + dictn[right]
-            if partner > 100:
-                print([left])
-                del dictn[left]
-            else:
-                print([left, right])
-                del dictn[left]
-                del dictn[right]
-        if dictn == {}:
-            break
+class Person(Item):
+    def __init__(self, name, weight):
+        super().__init__(name, weight)
+        self.sentient = True
 
-#Question:
-peep = {'Roman':90, 'Verlyn':80, 'Lloyd':60, 'Robin':40, 'Supplies':20}
+def cross_river(land1, boat = [], land2 = [], counter = 0, capacity = 100):
 
-#ANSWER:
-boat(peep)
+    for i in land1:
+        print([i.name, i.weight])
+    print("...wants to cross in a boat with a capacity of", capacity)
+    print()
+    
+    #Check for people who exceeds capacity
+    for i in land1:
+        if i.weight > capacity:
+            print(i.name, "can not cross")
+            land1.remove(i)
+    print()
+    
+    #Show people in land1
+    print("--------------------")
+    print("Current people in land 1")
+    for i in land1:
+        print([i.name, i.weight])
+    print()
+    
+    #Show people in land2
+    print("Current people in land 2")
+    for i in land2:
+        print([i.name, i.weight])
+    print("--------------------")
+    print()
+    
+    #Check for head count
+    heads = len(land1)
+    
+    #The loop
+    while land1 != []:
+        for i in land1:
+            #Sort group in ascending order
+            land1.sort(key = lambda x : x.weight)
+            
+            #1 person should be in the boat
+            for i in land1:
+                if i.sentient == True:
+                    if i.weight <= capacity:
+                        boat.append(i)
+                        counter += i.weight
+                        land1.remove(i)
+                        break
+                    else: continue
+            
+            #To find people to go with the person
+            for i in land1:
+                if counter + i.weight <= capacity:
+                    counter += i.weight
+                    boat.append(i)
+                    land1.remove(i)
+            if len(boat) == 1:
+                remove = boat.pop()
+                boat.append(land1[0])
+                del land1[0]
+                land1.append(remove)
+            
+            #print initial boat
+            for i in boat:
+                print([i.name, i.weight])
+            print("is crossing in a boat")
+            print()
+            
+            #Send boat load to land2
+            for i in boat:
+                land2.append(i)
+            boat.clear()
+            
+            #Print land2
+            for i in land2:
+                print([i.name, i.weight])
+            print("...crossed")
+            print()
+            
+            #Check if done
+            if len(land2) == heads:
+                #Show people in land1
+                print("--------------------------------------------------------------")
+                print("Current people in land 1")
+                for i in land1:
+                    print([i.name, i.weight])
+                print()
+                
+                #Show people in land2
+                print("Current people in land 2")
+                for i in land2:
+                    print([i.name, i.weight])
+                print("--------------------------------------------------------------")
+                print()
+                break
+            
+            #Reset
+            counter = 0
+            
+            #1 person of least weight should go back
+            land2.sort(key = lambda x : x.weight)
+            for i in land2:
+                if i.sentient == True:
+                    land1.append(i)
+                    land2.remove(i)
+                    print([i.name], "went back")
+                    print()
+                    break
+            
+            #Show people in land1
+            print("--------------------------------------------------------------")
+            print("Current people in land 1")
+            for i in land1:
+                print([i.name, i.weight])
+            print()
+            
+            #Show people in land2
+            print("Current people in land 2")
+            for i in land2:
+                print([i.name, i.weight])
+            print("--------------------------------------------------------------")
+            print()
+
+#-----------------------------------------------------------------------------
+
+Roman = Person("Roman", 90)
+Victor = Person("Victor", 120)
+Verlyn = Person("Verlyn", 80)
+Lloyd = Person("Lloyd", 60)
+Robin = Person("Robin", 40)
+Supplies = Item("Supplies", 20)
+crosser = [Roman, Verlyn, Lloyd, Robin, Supplies]
+
+cross_river(crosser)
